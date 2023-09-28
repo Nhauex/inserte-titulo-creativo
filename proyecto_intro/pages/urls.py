90f8ee064b453e7bb130d4b66e22431b20c4fcdb
Nhauex <73442725+Nhauex@users.noreply.github.com>
2023-09-27 21:58:27 -0300

    Arreglo de sintaxis y ortografia

--- proyecto_intro/pages/urls.py
+++ proyecto_intro/pages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 #Configuracion de url, para mapear la funcion decirhola a la url testeo/hola
 urlpatterns = [
-    path('', views.decir_hola)
+    path('', views.decir_hola),
     path('paginareciclaje.html', views.reciclaje)
 ]
