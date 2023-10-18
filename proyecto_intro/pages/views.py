207c999dd2ba1e6e2d8882651971a1298cb47689
Nhauex <73442725+Nhauex@users.noreply.github.com>
2023-10-18 20:07:20 -0300

    Sistema basico de logeo

--- proyecto_intro/pages/views.py
+++ proyecto_intro/pages/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 #Las funciones view reciben una request (pedido) y dan una  response (respuesta)
 #Ej (renderiza html, el html lo encuentran en la carpeta templates):
@@ -7,5 +8,13 @@ def decir_hola(req):
     return render(req, 'algo.html')
 
 #Podemos asignarle una Url a esta view -> vease urls.py
+@login_required
 def reciclaje(req):
     return render(req, 'herramienta1.html' )
+
+def login(req):
+    return render(req, 'login.html')
+
+def exit(req):
+    logout(req)
+    return redirect('home')
