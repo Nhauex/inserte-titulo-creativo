13d5b8bc1035756831231baa4cbff889c387cc8f
Nanae <dojeda@usm.cl>
2023-11-04 22:01:39 -0300

    "cambios?"

--- proyecto_intro/proyecto_intro/settings.py
+++ proyecto_intro/proyecto_intro/settings.py
@@ -40,9 +40,10 @@ INSTALLED_APPS = [
     'pages',
     'crispy_forms',
     'crispy_bootstrap5',
+    
 ]
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_ALLOWED_TEMPLATE_PACKS =  ['bootstrap5']
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
